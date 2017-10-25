cdfc47c695c199a94364e7b1516d36a3a0101c0f
陳維漢 <wikeyno@gmail.com>
2017-10-25 18:13:53 +0800

    hello world

---
+++ 20171025_HelloWorld/20171025_HelloWorld.js
@@ -0,0 +1,24 @@
+//-----  -----
+//----- Hello World -----
+console.log("Hello World!!");
+console.log("This is Kingchenkong.");
+console.log("node.js - console.log() Method");
+console.log("node.js - console.info() Method");
+
+//----- Console Format String -----
+console.log("%s", "this is KCK");
+console.log("%s %s %s", "aaa", "bbb", "ccc");
+console.log("%s:%s", "object", "string");
+console.log("%s-%s", "object", "string");
+console.log("%");
+console.log("%%");
+//----- Console Format Number -----
+console.log("%d", 1234567);
+console.log("%3d", 1234567);
+console.log("%d - %d = %d", 8.8, 8.0, (8.8 - 8));
+console.log(12345);
+console.log("%d", 8+8, 8-8, 8*8, 8/8);
+console.log("%d", "8+8");
+console.log("%d", 8/0);
+
+//
