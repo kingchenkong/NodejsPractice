7e8ab89b2fba8b388a7fe14e204a21476948a70f
陳維漢 <wikeyno@gmail.com>
2017-11-08 16:57:56 +0800

    node.js MySQL test and Demo test

---
+++ express/express-mysqltest/app.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+//----------------- DataBase -----------------
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "testdb01",
+  port: 3307
+});
+
+con.connect(function (err) {
+  if (err) {
+    console.log('connecting error');
+    return;
+  }
+  console.log('connecting success');
+});
+//----------------- -----------------
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//----------------- DataBase -----------------
+// db state
+app.use(function (req, res, next) {
+  req.con = con;
+  next();
+});
+//----------------- -----------------
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
